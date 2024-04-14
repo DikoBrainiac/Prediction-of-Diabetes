0ee38b1b90b224cc6be836d48730d1c695bf1297
DikoBrainiac <150687926+DikoBrainiac@users.noreply.github.com>
2024-04-14 21:47:29 +0100

    Update Diabetes_Streamlit.py

--- Diabetes_Streamlit.py
+++ Diabetes_Streamlit.py
@@ -11,6 +11,8 @@ from imblearn.over_sampling import SMOTE
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from sklearn.model_selection import cross_validate
 
+selected_features = ['age', 'smoking_history','bmi', 'HbA1c_level', 'blood_glucose_level']
+
 # Load the dataset
 @st.cache_data
 def load_data():
@@ -45,7 +47,6 @@ def train_model(df):
     X = df.drop(['diabetes'], axis=1)
     y = df['diabetes']
     # Feature selection
-    selected_features = ['age', 'smoking_history','bmi', 'HbA1c_level', 'blood_glucose_level']
     X_selected = X[selected_features]
     # Train-test split
     X_train, X_test, y_train, y_test = train_test_split(X_selected, y, test_size=0.2, random_state=42)
@@ -100,15 +101,15 @@ def train_model(df):
     rf_model = RandomForestClassifier(**params_rf)
     et_model = ExtraTreesClassifier(**params_et)
     ensemble_rf_et = VotingClassifier(estimators=[('rf', rf_model), ('et', et_model)], voting='hard')
-    # # Perform cross-validation with the ensemble classifier
-    # scoring = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc']
-    # ensemble_rf_et_score = cross_validate(ensemble_rf_et, X=X_train_scaled, y=y_train_res, cv=10, scoring=scoring, return_train_score=False)
-    # # Compute the mean scores
-    # mean_scores = {metric: np.mean(ensemble_rf_et_score[f'test_{metric}']) for metric in scoring}
-    # # Print mean scores
-    # print("Mean Scores:")
-    # for metric, score in mean_scores.items():
-    #     print(f"{metric}: {score:.4f}")
+    # Perform cross-validation with the ensemble classifier
+    scoring = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc']
+    ensemble_rf_et_score = cross_validate(ensemble_rf_et, X=X_train_scaled, y=y_train_res, cv=10, scoring=scoring, return_train_score=False)
+    # Compute the mean scores
+    mean_scores = {metric: np.mean(ensemble_rf_et_score[f'test_{metric}']) for metric in scoring}
+    # Print mean scores
+    print("Mean Scores:")
+    for metric, score in mean_scores.items():
+        print(f"{metric}: {score:.4f}")
     # Fit the ensemble model on the entire training set
     ensemble_rf_et.fit(X_train_scaled, y_train_res)
     return ensemble_rf_et, X_test_scaled, y_test, scaler, selected_features
@@ -255,16 +256,6 @@ elif section == 'Model Training':
     st.write(f'Recall: {recall:.2f}')
     st.write(f'F1 Score: {f1:.2f}')
     st.write(f'AUC: {auc:.2f}')
-
-    # # Perform cross-validation with the ensemble classifier
-    # scoring = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc']
-    # ensemble_rf_et_score = cross_validate(ensemble_rf_et, X=X_train_scaled, y=y_train_res, cv=10, scoring=scoring, return_train_score=False)
-
-    # # Compute and print mean scores
-    # mean_scores = {metric: np.mean(ensemble_rf_et_score[f'test_{metric}']) for metric in scoring}
-    # st.write('Mean Scores:')
-    # for metric, score in mean_scores.items():
-    #     st.write(f'{metric}: {score:.4f}')
 
     # Confusion matrix visualization
     cm = confusion_matrix(y_test, y_pred)
